78406912f7979c689baf212038e4abc8c8828b4e
Edgar Agno <edgardo76@yahoo.com>
2025-08-09 12:24:55 +0100

    Convenience initialiser

--- Sources/DogAPI/Model/DogBreed.swift
+++ Sources/DogAPI/Model/DogBreed.swift
@@ -4,4 +4,10 @@ public struct DogBreed: Equatable, Codable {
     public let base: String?
     public let name: String
     public let subBreeds: [DogBreed]?
+    
+    init(base: String? = nil, name: String, subBreeds: [DogBreed]? = nil) {
+        self.base = base
+        self.name = name
+        self.subBreeds = subBreeds
+    }
 }
